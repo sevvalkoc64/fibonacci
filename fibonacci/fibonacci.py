5263a055732b0ed5a4d4ff357bf216469815c238
sevvalkoc64 <kocsevval963@gmail.com>
2024-12-23 19:50:41 +0300

    ilk commit

---
+++ fibonacci/fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    fibonacci_dizisi = [0, 1]
+
+    for i in range(2, n):
+        sonraki = fibonacci_dizisi[-1] + fibonacci_dizisi[-2]
+        fibonacci_dizisi.append(sonraki)
+
+    return fibonacci_dizisi
+
+print(fibonacci(10))
